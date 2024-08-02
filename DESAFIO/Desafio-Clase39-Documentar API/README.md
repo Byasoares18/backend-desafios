d48d65f6666d455c24c2f95c0c87b1912f1e1991
Byasoares18 <105671476+Byasoares18@users.noreply.github.com>
2024-08-01 21:49:25 -0300

    edit

---
+++ DESAFIO/Desafio-Clase39-Documentar API/README.md
@@ -0,0 +1,3 @@
+Proyecto final del curso de BackEnd
+
+Para iniciar el proyecto deben correr el comando "npm install" y luego el script "npm run dev" el cual corre el comando "nodemon ./src/app.js --persistence MONGO"
--- DESAFIO/Desafio-Clase39-Documentar API/src/app.js
+++ DESAFIO/Desafio-Clase39-Documentar API/src/app.js
@@ -1,49 +1,45 @@
 import express from 'express'
 import handlebars from 'express-handlebars'
-import mongoose from 'mongoose'
 import {Server} from 'socket.io'
 import __dirname from './utils.js'
 import routerProducts from './routes/products.router.js'
 import routerCarts from './routes/carts.router.js'
 import routerViews from './routes/views.router.js'
 import routerSessions from './routes/session.router.js'
-import session from 'express-session'
-import mongoStore from 'connect-mongo'
+import routerUser from './routes/user.router.js'
 import passport from 'passport'
 import initializePassport from './config/passport.config.js'
 import config from './config/config.js'
-
-
-
+import cookieParser from 'cookie-parser'
+import mockingRouter from './routes/mocking.router.js'
+import { addLogger } from './middlewares/middlewares.js'
+import swaggerJsdoc from 'swagger-jsdoc'
+import swaggerUiExpress from 'swagger-ui-express'
+ 
 const app = express()
-
-
-app.use(session({
-    store: mongoStore.create({
-        mongoUrl: config.mongoURL,
-        dbName: config.mongoDBName,
-        ttl:100
-    }),
-    secret: 'secret',
-    resave: true,
-    saveUninitialized: true,
-}))
-
-mongoose.connect(config.mongoURL, {dbName: config.mongoDBName})
-    .then(() => {
-        console.log('DB connected. ✊')
-    })
-    .catch(e => {
-        console.error('Error connecting to DB 😓')
-    })
 
 initializePassport()
 
+const swaggerOptions = {
+    definition:{
+        openapi:'3.0.1',
+        info:{
+            title: 'Documentacion de API para E-commerce',
+            description: "API creada para un e-commerce de Zapatillas"
+        }
+    },
+    apis:[`${__dirname}/docs/**/*.yaml`]
+}
+
+const specs = swaggerJsdoc(swaggerOptions)
+app.use('/apidocs', swaggerUiExpress.serve, swaggerUiExpress.setup(specs))
+
+app.use(cookieParser(config.blockCookie))
 app.use(passport.initialize())
-app.use(passport.session())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use('/static', express.static(__dirname + '/public'))
+app.use(addLogger)
 
 app.engine('handlebars', handlebars.engine())
 app.set('views',__dirname + '/views')
@@ -53,9 +49,8 @@ app.use('/api/products', routerProducts)
 app.use('/api/carts', routerCarts)
 app.use('/api/sessions', routerSessions)
 app.use('/home', routerViews)
-
-
-
+app.use('/api/user', routerUser)
+app.use('/api/mocking', mockingRouter)
 
 const http = app.listen(config.port, () => console.log(`Servidor corriendo en el puerto ${config.port}`))
 export const socketServer = new Server(http)
